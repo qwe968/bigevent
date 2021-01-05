f9f3731f508a4d7dc0be6b52269536eae68bc542
wy <190730@qq.com>
2021-01-05 14:33:09 +0800

    user模块完成

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,60 @@
+$(function () {
+    let form = layui.form
+    let layer = layui.layer
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '昵称长度必须在1~6个字符之间!'
+            }
+        }
+    })
+
+    initUserInfo()
+    function initUserInfo() {
+        $.ajax({
+            type: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败')
+                }
+                console.log(res);
+                form.val('formUserInfo', res.data)
+            }
+        })
+
+    }
+
+
+
+
+    $('#btnReset').on('click', function (e) {
+        e.preventDefault();
+        initUserInfo();
+    })
+
+
+
+    $(".layui-form").on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('修改用户信息失败')
+                }
+                layer.msg('修改用户信息成功')
+
+                window.parent.getUserInfo();
+            }
+        })
+
+
+
+
+
+    })
+
+})
